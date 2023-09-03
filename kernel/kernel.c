bf5d95c1e762259382c79e87380c123ddd6482ea
lightswisp <you@example.com>
2023-09-03 02:23:51 -0400

    yellow bg

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -1,20 +1,23 @@
-void printA(char* str){
-	char* video_memory = (char*) 0xb8000;
-	str--;
-	video_memory[3] = str;	
-	//*video_memory = 'A';
+#define VIDEO_MEMORY 0xb8000
+#define MAX_ROWS 25
+#define MAX_COLS 80
+
+void clearScreen(char character, int color){
+	unsigned short* video_memory = (unsigned short*) VIDEO_MEMORY;	
+	int i = 0;
+
+	while(i <= (MAX_COLS * MAX_ROWS) + 2){
+		video_memory[i] = (int)character;
+		video_memory[i+1] = color;
+		i++;
+	}
+
 }
 
-void main(){
-	int x = 2*4;
-  char* video_memory = (char*) 0xb8000;
-  *video_memory = (char)(48 + x);
-  printA('Hell');
-  *video_memory = 'B';
-  printA('Hell');
-	// video_memory += 2;
-	// *video_memory = 'a';
-//	*video_memory = (char)49;
+int main(){
+	int color = 0xEE;
+	char character = ' ';
+	clearScreen(character, color);
 	while(1);
 }
 
